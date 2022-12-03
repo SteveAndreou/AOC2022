ff5b9ccb90006fece0f6a457ab44928e28155397
Steve Andreou <stephen.andreou@gmail.com>
2022-12-03 14:41:22 +1100

    Solution 01

--- src/01/01.ts
+++ src/01/01.ts
@@ -1,1 +1,26 @@
-console.log("hello world");
+import * as fs from 'fs';
+
+//Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+try {
+    const filename = `${__dirname}/input.txt`;
+    const calories = fs.readFileSync(filename, 'utf-8');
+    const elfList = calories.split('\n\n');
+
+    let max = 0;
+
+    for (let index = 0; index < elfList.length; index++) {
+        const elf = elfList[index];
+
+        const elfItems = elf.split('\n');
+        const cals = elfItems.reduce((prev, item) => prev + Number(item), 0);
+
+        if (cals > max) {
+            max = cals;
+        }
+    }
+
+    console.log(`Max calories: ${max}`);
+} catch (ex) {
+    console.error('❌ uh oh');
+    console.error(ex);
+}
